86647afd218843c115bb1e6269fb81269cf7d872
adrian.pi <Danielpiveta257@gmail.com>
2025-04-15 17:15:10 -0300

    Nivelamento em programação

--- Nivelamento em programação/aula1 - Copia.py
+++
@@ -1,4 +0,0 @@
-nome = "Hello World!"
-nome = "Adrian"
-idade = 23
-print("O nome digitado foi: ", nome, " e a idade foi: ", idade, ".")
---
+++ Nivelamento em programação/aula3.py
@@ -0,0 +1,13 @@
+def mostrarNome():
+    nome  =input("Digite seu nome: ")
+    print("Nome digitado: " + nome)
+
+def mostrarNome(nome):
+    print("Nome transferido: " + nome)
+
+def mostrarIdade():
+    idade = input("Digite sua idade: ")
+    print("Idade: " + idade)
+
+mostrarIdade()
+mostrarNome()
